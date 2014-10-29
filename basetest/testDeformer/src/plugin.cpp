084bf619ff2872f66bdafe9266ee1589df3d90f1
yaoyansi <yaoyansi@aliyun.com>
2014-10-29 21:44:33 +0800

    basetest| testDeformer|: +cpp files

---
+++ basetest/testDeformer/src/plugin.cpp
@@ -0,0 +1,46 @@
+#include <maya/MFnPlugin.h>
+#include <maya/MStatus.h>
+
+#include "testDeformer.h"
+
+// These methods load and unload the plugin, registerNode registers the
+// new node type with maya
+//
+MStatus initializePlugin( MObject obj )
+{
+
+	MFnPlugin plugin( obj, PLUGIN_COMPANY, "4.5", "Any" );
+
+	CHECK_MSTATUS(
+		plugin.registerNode(
+            DynamicEnum::cTypeName(),
+            DynamicEnum::cTypeId(),
+            DynamicEnum::creator,
+            DynamicEnum::initialize,
+            DynamicEnum::cType(),
+            &DynamicEnum::cClassification()
+		) );
+
+	MString command( "if( `window -exists createRenderNodeWindow` ) {refreshCreateRenderNodeWindow(\"" );
+	command += DynamicEnum::cClassification();
+	command += "\");}\n";
+
+	CHECK_MSTATUS( MGlobal::executeCommand( command ) );
+
+	return( MS::kSuccess );
+}
+
+MStatus uninitializePlugin( MObject obj )
+{
+	MFnPlugin plugin( obj );
+
+	CHECK_MSTATUS( plugin.deregisterNode( DynamicEnum::cTypeId() ) );
+
+	MString command( "if( `window -exists createRenderNodeWindow` ) {refreshCreateRenderNodeWindow(\"" );
+	command += DynamicEnum::cClassification();
+	command += "\");}\n";
+
+	CHECK_MSTATUS( MGlobal::executeCommand( command ) );
+
+	return MS::kSuccess;
+}
