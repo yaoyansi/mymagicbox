26ed6c98e0fe41ba7be54244cdb9bda1d8b45fd8
yaoyansi <yaoyansi@aliyun.com>
2014-11-08 14:03:06 +0800

    basetest| testDeformer|: update doc

---
+++ basetest/testDeformer/build/cmake_build/CMakeFiles/testDeformer.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/user0/dev/mymagicbox/basetest/testDeformer/src/plugin.cpp" "/home/user0/dev/mymagicbox/basetest/testDeformer/build/cmake_build/CMakeFiles/testDeformer.dir/home/user0/dev/mymagicbox/basetest/testDeformer/src/plugin.cpp.o"
+  "/home/user0/dev/mymagicbox/basetest/testDeformer/src/testDeformer.cpp" "/home/user0/dev/mymagicbox/basetest/testDeformer/build/cmake_build/CMakeFiles/testDeformer.dir/home/user0/dev/mymagicbox/basetest/testDeformer/src/testDeformer.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "LINUX"
+  "LINUX_64"
+  "REQUIRE_IOSTREAM"
+  "_BOOL"
+  "_LINUX"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/autodesk/maya2015-x64/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ basetest/testDeformer/build/cmake_build/CMakeFiles/testDeformer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/testDeformer.dir/home/user0/dev/mymagicbox/basetest/testDeformer/src/testDeformer.cpp.o"
+  "CMakeFiles/testDeformer.dir/home/user0/dev/mymagicbox/basetest/testDeformer/src/plugin.cpp.o"
+  "/home/user0/dev/mymagicbox/basetest/testDeformer/plug-ins/testDeformer.pdb"
+  "/home/user0/dev/mymagicbox/basetest/testDeformer/plug-ins/testDeformer.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/testDeformer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
