006ca6b7e206d80001fe15a1251976a3ca6f093c
yaoyansi <yaoyansi@aliyun.com>
2014-11-12 17:04:22 +0800

    common|: +node_ids.h, copyright.h

---
+++ common/copyright.h
@@ -0,0 +1,4 @@
+
+#pragma once
+
+#define PLUGIN_AUTHOR	"YaoYu"
---
+++ common/node_ids.h
@@ -0,0 +1,45 @@
+/*
+How to request your own Maya NodeIds?
+
+Access http://www.autodesk.com/developmaya and go to 'Register a Maya Developer Node ID Block' section.  
+You'll find a link there to self-register your ID block. 
+
+*/
+
+#pragma once
+
+// 0x00122240:0x0012224F
+#define	NodeID_node_template	0x00122240
+#define	NodeID_mystring		0x00122241
+#define	NodeID_dynamic_enum	0x00122242
+#define	NodeID_tornado_field	0x00122243
+
+// 0x00122250:0x0012225F
+// 0x00122260:0x0012226F
+// 0x00122270:0x0012227F
+// 0x00122280:0x0012228F
+// 0x00122290:0x0012229F
+// 0x001222A0:0x001222AF
+// 0x001222B0:0x001222BF
+// 0x001222C0:0x001222CF
+// 0x001222D0:0x001222DF
+// 0x001222E0:0x001222EF
+// 0x001222F0:0x001222FF
+// 0x00122300:0x0012230F
+// 0x00122310:0x0012231F
+// 0x00122320:0x0012232F
+// 0x00122330:0x0012233F
+
+
+
+
+
+
+
+
+
+
+
+
+
+
