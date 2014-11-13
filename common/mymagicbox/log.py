c9238e162bab1520809f1e34d0c8e512a236d2db
yaoyansi <yaoyansi@aliyun.com>
2014-11-13 15:11:44 +0800

    common|: log_debug() --> debug()

--- common/mymagicbox/log.py
+++ common/mymagicbox/log.py
@@ -1,30 +1,30 @@
 
 #
-def log_fatal(format, *params):
+def fatal(format, *params):
     print("mymagicbox> Fatal> "+format % params);
 
-def log_err(format, *params):
+def err(format, *params):
     print("mymagicbox> Err>   "+format % params);
 
-def log_warn(format, *params):
+def warn(format, *params):
     print("mymagicbox> Warn>  "+format % params);
 
-def log_info(format, *params):
+def info(format, *params):
     print("mymagicbox> Info>  "+format % params);
 
-def log_debug(format, *params):
+def debug(format, *params):
     print("mymagicbox> Debug> "+format % params);
 
 #
 if __name__ == "__main__":
-	log_fatal("msg: %s", "aaa");
-	log_fatal("msg: %s", 123);
-	log_err("msg: %s", "aaa");
-	log_err("msg: %s", 123);
-	log_warn("msg: %s", "aaa");
-	log_warn("msg: %s", 123);
-	log_info("msg: %s", "aaa");
-	log_info("msg: %s", 123);
-	log_debug("msg: %s", "aaa");
-	log_debug("msg: %s", 123);
+	fatal("msg: %s", "aaa");
+	fatal("msg: %s", 123);
+	err("msg: %s", "aaa");
+	err("msg: %s", 123);
+	warn("msg: %s", "aaa");
+	warn("msg: %s", 123);
+	info("msg: %s", "aaa");
+	info("msg: %s", 123);
+	debug("msg: %s", "aaa");
+	debug("msg: %s", 123);
 
