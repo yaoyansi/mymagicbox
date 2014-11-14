7e55e019ca4e06a3af802ba2edfd8cf1f673dc9d
yaoyansi <yaoyansi@aliyun.com>
2014-11-14 23:47:48 +0800

    AETemplate|: use imp.load_source() to load the AE*Template.py

--- version_control/scripts/userSetup.py
+++ version_control/scripts/userSetup.py
@@ -6,10 +6,10 @@ import sys
 import maya.cmds as cmds
 
 modulePath = cmds.getModulePath(moduleName='version_control')
+modulePath = modulePath.replace('\\','/')
 
 # get fullpath of current py file
 this_file_path = modulePath + '/scripts/userSetup.py';
-this_file_path = this_file_path.replace('\\','/')
 print("this is: "+this_file_path);
 
 # add common module to python path
@@ -17,7 +17,8 @@ commonPath = modulePath+'/../common';
 sys.path.insert(0, commonPath);
 
 """
-2. When pymel is imported it will automatically import the module ``AETemplates``, if it exists.
-So I have to import pymel manully to regist my AETemplates which are located at ./AETemplates
+2. There are so many ``AETemplates`` directories, so I have to load the modules with the full path.
 """
-import pymel.core
+import imp
+imp.load_source('AETemplates.AEtestNodeATemplate', modulePath+'/scripts/AETemplates/AEtestNodeATemplate.py')
+imp.load_source('AETemplates.AEtestNodeBTemplate', modulePath+'/scripts/AETemplates/AEtestNodeBTemplate.py')
